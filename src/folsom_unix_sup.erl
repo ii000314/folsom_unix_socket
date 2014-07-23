35b326f4caebb190c768ce26c1aa17790bffe462
Kirill Pinchuk <k_pinchuk@wargaming.net>
2014-07-23 16:29:52 +0300

    Remove unused module

--- src/folsom_unix_sup.erl
+++ src/folsom_unix_sup.erl
@@ -1,6 +1,6 @@
 -module(folsom_unix_sup).
 -behaviour(supervisor).
--export([start_link/0, init/1]).
+-export([start_link/0, init/1, check_childs/0]).
 
 
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
@@ -9,7 +9,16 @@ start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-  FUnix = ?CHILD(folsom_unix, worker),
   GUnix = ?CHILD(gen_unix, worker),
-  {ok, { {one_for_one, 5, 10}, [FUnix, GUnix]} }.
+  {ok, { {one_for_one, 5, 10}, [GUnix]} }.
+
+check_childs() ->
+  [check_for_terminate(Id) || {Id, _, _, _} <- supervisor:which_children(?MODULE)].
+
+check_for_terminate(folsom_unix) ->
+  supervisor:terminate_child(?MODULE, folsom_unix),
+  supervisor:delete_child(?MODULE, folsom_unix),
+  ok;
+check_for_terminate(_) ->
+  ok.
 
--- src/gen_unix.erl
+++ src/gen_unix.erl
@@ -11,6 +11,7 @@
 
 -include_lib("procket/include/procket.hrl").
 -record(st, {sock, poll}).
+-vsn("1.1").
 
 start_link() ->
    gen_server:start_link(?MODULE, [], []).
@@ -37,6 +38,8 @@ accept_loop(LSock, BackPid, Poll) ->
 
 init([]) ->
   process_flag(trap_exit, true),
+  folsom_metrics:new_gauge(<<"running">>),
+  folsom_metrics:notify(<<"running">>, <<"1">>),
   {SockPath, SockName} = get_path(),
   {ok, Socket} = procket:socket(?PF_LOCAL, ?SOCK_STREAM, 0),
   {ok, Poll} = inert:start(),
@@ -49,13 +52,13 @@ init([]) ->
   {ok, State}.
 
 handle_call(Req, _From, State) ->
-  lager:debug("Unhandled call ~p~n", [Req]),
+  lager:warning("Unhandled call ~p~n", [Req]),
   {reply, State}.
 
 handle_cast(stop, State) ->
   {stop, normal, State};
 handle_cast(Req, State) ->
-  lager:debug("Unhandled cast: ~p~n", [Req]),
+  lager:warning("Unhandled cast: ~p~n", [Req]),
   {noreply, State}.
 
 handle_info({ready, Sock}, #st{poll=Poll}=State) ->
@@ -63,7 +66,10 @@ handle_info({ready, Sock}, #st{poll=Poll}=State) ->
   case procket:recv(Sock, 1024) of
     {ok, Req} ->
       lager:debug("Read req ~p", [Req]),
-      gen_server:cast(folsom_unix, {req, Req, Sock, self()});
+      Val = folsom_metrics:get_metric_value(Req),
+      Resp = make_resp(Val),
+      procket:write(Sock, Resp),
+      procket:close(Sock);
     {error,eagain} ->
       lager:debug("Got eagain. Try read again"),
       self() ! {ready, Sock};
@@ -71,18 +77,29 @@ handle_info({ready, Sock}, #st{poll=Poll}=State) ->
       lager:error("During read from unix socket: ~p", [Error]),
       procket:write(Sock, <<"-1">>) end,
   {noreply, State};
-handle_info({resp, Resp, Sock}, State) ->
-  procket:write(Sock, Resp),
-  procket:close(Sock),
-  {noreply, State};
 handle_info(Info, State) ->
-  lager:debug("Unhandled info: ~p~n", [Info]),
+  lager:warning("Unhandled info: ~p~n", [Info]),
   {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
+  catch folsom_unix_sup:check_childs(),
   {ok, State}.
 
 terminate(_Reason, #st{sock=Sock, poll=Poll}=_State) ->
   procket:close(Sock),
   inert:stop(Poll),
   ok.
+
+make_resp(Val) when is_binary(Val) ->
+  Val;
+make_resp(Val) when is_integer(Val) ->
+  list_to_binary(integer_to_list(Val));
+make_resp([_, {one, Val} | _]) ->
+  list_to_binary(float_to_list(Val, [{decimals, 5}]));
+make_resp(Val) when is_list(Val) ->
+  list_to_binary(Val);
+make_resp({error, Metric, nonexistent_metric}) ->
+  lager:warning("Call to nonexistend_metric ~p", [Metric]),
+  <<"0">>;
+make_resp(_) ->
+  <<"0">>.
