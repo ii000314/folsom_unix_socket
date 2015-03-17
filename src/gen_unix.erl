875cc6a25f944a76d8f5339fe475a09e0d81423a
Yuriy Zhloba <y_zhloba@wargaming.net>
2015-03-17 16:32:52 +0300

    support folsom histogram statistics

--- src/gen_unix.erl
+++ src/gen_unix.erl
@@ -32,7 +32,7 @@ accept_loop(LSock, BackPid, Poll) ->
     {ok, RSock} ->
       BackPid ! {ready, RSock};
     {error, Rsn} ->
-      lager:error("Got error ~p", [Rsn]),
+      lager:error("folsom_unix, accept_loop error ~p", [Rsn]),
       timer:sleep(250) end,
   ?MODULE:accept_loop(LSock, BackPid, Poll).
 
@@ -52,33 +52,41 @@ init([]) ->
   {ok, State}.
 
 handle_call(Req, _From, State) ->
-  lager:warning("Unhandled call ~p~n", [Req]),
+  lager:error("folsom_unix, Unhandled call ~p~n", [Req]),
   {reply, State}.
 
 handle_cast(stop, State) ->
   {stop, normal, State};
 handle_cast(Req, State) ->
-  lager:warning("Unhandled cast: ~p~n", [Req]),
+  lager:error("folsom_unix, Unhandled cast: ~p~n", [Req]),
   {noreply, State}.
 
 handle_info({ready, Sock}, #st{poll=Poll}=State) ->
   inert:poll(Poll, Sock, [{timeout, 250}]),
   case procket:recv(Sock, 1024) of
     {ok, Req} ->
-      lager:debug("Read req ~p", [Req]),
-      Val = folsom_metrics:get_metric_value(Req),
+      Val = case binary:split(Req, <<" ">>, [global]) of
+                [Name] -> folsom_metrics:get_metric_value(Name);
+                [Name, <<"stat">>, Param] ->
+                    Stat = lists:map(fun({K, V}) -> {list_to_binary(atom_to_list(K)), V} end,
+                                     folsom_metrics:get_histogram_statistics(Name)),
+                    proplists:get_value(Param, Stat, 0.0);
+                _ -> lager:error("folsom_unix, invalid request ~p", [Req]),
+                     0
+            end,
       Resp = make_resp(Val),
+      lager:debug("folsom_unix, req~p resp:~p", [Req, Resp]),
       procket:write(Sock, Resp),
       procket:close(Sock);
     {error,eagain} ->
-      lager:debug("Got eagain. Try read again"),
       self() ! {ready, Sock};
     Error ->
-      lager:error("During read from unix socket: ~p", [Error]),
+      lager:error("folsom_unix, error during read from unix socket: ~p", [Error]),
       procket:write(Sock, <<"-1">>) end,
   {noreply, State};
+
 handle_info(Info, State) ->
-  lager:warning("Unhandled info: ~p~n", [Info]),
+  lager:error("folsom_unix, unhandled info: ~p~n", [Info]),
   {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -94,14 +102,16 @@ make_resp(Val) when is_binary(Val) ->
   Val;
 make_resp(Val) when is_integer(Val) ->
   list_to_binary(integer_to_list(Val));
-make_resp([_, {one, Val} | _]) when is_float(Val) ->
+make_resp(Val) when is_float(Val) ->
   erlang:list_to_binary(float_to_list(Val, [{decimals, 5}]));
+make_resp([_, {one, Val} | _]) when is_float(Val) ->
+  make_resp(Val);
 make_resp([_, {one, Val} | _ ]) when is_integer(Val) ->
   list_to_binary(integer_to_list(Val));
 make_resp(Val) when is_list(Val) ->
   list_to_binary(Val);
 make_resp({error, Metric, nonexistent_metric}) ->
-  lager:warning("Call to nonexistend_metric ~p", [Metric]),
+  lager:error("folsom_unix, call to nonexistend_metric ~p", [Metric]),
   <<"0">>;
 make_resp(_) ->
   <<"0">>.
